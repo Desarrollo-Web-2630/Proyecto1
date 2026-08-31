6ecc92c8e611a11a6a5b79d3c7dabe41a56379f4
Mariana Osorio <mariana-osoriov@javeriana.edu.co>
2026-08-31 16:22:51 -0500

    Servicio gateway

--- thymeleaf/src/main/java/com/proyecto1/thymeleaf/repository/ProcesoRepository.java
+++ thymeleaf/src/main/java/com/proyecto1/thymeleaf/repository/ProcesoRepository.java
@@ -1,13 +1,17 @@
 package com.proyecto1.thymeleaf.repository;
 
+import com.proyecto1.thymeleaf.model.Gateway;
 import com.proyecto1.thymeleaf.model.Proceso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProcesoRepository extends JpaRepository<Proceso, Long> {
 
     List<Proceso> findByEmpresaId(Long empresaId);
 
     boolean existsByNombreAndEmpresaId(String nombre, Long empresaId);
+
+    Optional<Proceso> findByIdAndEmpresaId(Long procesoId, Long empresaId);
 }
